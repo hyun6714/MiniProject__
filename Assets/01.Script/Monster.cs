21710e4bd5cbc9e8b70ee5826854fb6e692fdaf9
배수현 <byhzty@gmail.com>
2026-07-13 11:19:45 +0900

    3

--- Assets/01.Script/Monster.cs
+++ Assets/01.Script/Monster.cs
@@ -22,11 +22,6 @@ public class Monster : MonoBehaviour
 
     Vector3 targetP;
 
-    // 만들어야 하는거 몬스터 랜덤위치 스폰(5마리 정도)(스포너 오브젝트 따로 만들고 프리팹등록)
-    // 만약 닿은 물체가 플레이어 라면 게임펄 상태로 돌리기
-    // 죽었을 때 점수 증가 
-    // 상태 변환 (공격 맞았을 시 정지 상태 및 공격안에 갇히기)
-
     void Start()
     {
         mstate = MonsterState.Move;
