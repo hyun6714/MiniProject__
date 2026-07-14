b742299c068b88f0375d2ac733017e0a3cd7f259
배수현 <byhzty@gmail.com>
2026-07-14 10:28:26 +0900

    3

--- Assets/01.Script/BubleManager.cs
+++ Assets/01.Script/BubleManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BubleManager : MonoBehaviour
 {
-    [SerializeField] GameObject Buble;
+    [SerializeField] GameObject BublePrefab;
 
     int bublemax;
     float attackDmg;
