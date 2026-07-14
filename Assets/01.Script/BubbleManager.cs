6c56f38d706be4aadd0d613792b92fb112c54d05
배수현 <byhzty@gmail.com>
2026-07-14 11:11:45 +0900

    4

--- Assets/01.Script/BubbleManager.cs
+++ Assets/01.Script/BubbleManager.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Rendering;
 public class BubbleManager : MonoBehaviour
 {
     [SerializeField] private GameObject bubblePrefab;
