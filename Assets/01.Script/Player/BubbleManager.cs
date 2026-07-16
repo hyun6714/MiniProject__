e67d91f24198c7b12489501358b3fc454cb51fb3
배수현 <byhzty@gmail.com>
2026-07-16 15:21:18 +0900

    6

--- Assets/01.Script/Player/BubbleManager.cs
+++ Assets/01.Script/Player/BubbleManager.cs
@@ -6,9 +6,15 @@ public class BubbleManager : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private GameObject[] bubblePool;
     [SerializeField] private int poolSize;
+    SpriteRenderer targetsr;
 
     void Start()
     {
+        if(target != null)
+        {
+            targetsr = target.GetComponent<SpriteRenderer>();
+        }
+        
         poolSize = 10;
         bubblePool = new GameObject[poolSize];
         for(int i=0; i<poolSize; i++)
@@ -31,11 +37,9 @@ public class BubbleManager : MonoBehaviour
             {
                 if (!bubblePool[i].activeSelf)
                 {
-                    float direction = Mathf.Sign(target.localScale.x);
+                    float direction = targetsr.flipX ? -1.5f : 1.5f; 
 
-                    Vector3 spawnOffset = new Vector3 (1.5f*direction, 0, 0);
-
-                    bubblePool[i].transform.position = target.position + spawnOffset;
+                    bubblePool[i].transform.position = target.position + new Vector3(direction,0,0);
                     bubblePool[i].SetActive(true);
                     break;
                 }
