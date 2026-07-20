4a939e2a9e8d5858b73de84f086c28a64556d9a5
배수현 <byhzty@gmail.com>
2026-07-20 10:51:45 +0900

    7

--- Assets/01.Script/Potal.cs
+++ Assets/01.Script/Potal.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Potal : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
